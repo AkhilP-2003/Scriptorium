d241b532031132bc4cfd78f22a325b3160eaf0da
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2024-10-26 01:58:45 -0400

    refresh.js

--- pages/api/users/refresh.js
+++ pages/api/users/refresh.js
@@ -2,7 +2,24 @@ import { prisma } from "@/prisma/client";
 import { verifyRefreshToken, generateAccessToken} from "@/utils/auth";
 
 export default function handler(req, res) {
-    
+    if (req.method === "POST") {
+        const {refreshToken} = req.body
+        if (!refreshToken) {
+            return res.status(400).json({error: "Invalid credentials"});
+        }
+        const refresh = verifyRefreshToken(refreshToken);
+        if (!refresh) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+        // Generate a new access token using the payload from the refresh token
+        const newAccessToken = generateAccessToken({ userName: refresh.userName, role: refresh.role });
 
+        // Return the new access token in the response
+        return res.status(200).json({
+            "accessToken": newAccessToken,
+        });
+    }else {
+        return res.status(400).json({ error: "Method Not Allowed" });
+    }
 
 }
