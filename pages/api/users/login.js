2f0e74ecc382a7abb6aedd9e2b7f138e39e636cf
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2024-10-26 01:37:44 -0400

    initial login/signup files, auth.js file

---
+++ pages/api/users/login.js
@@ -0,0 +1,31 @@
+import { prisma } from "@/prisma/client";
+import { comparePassword, generateRefreshToken, generateAccessToken } from "@/utils/auth";
+
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        const {userName, password} = req.body;
+        
+        if (!userName || !password) {
+            return res.status(401).json({error: "Please provide all fields"});
+        }
+        const existingUser = await prisma.user.findUnique({
+            where: {
+              userName,
+            },
+          });
+        
+        if (!existingUser || !(await comparePassword(password, existingUser.password))) {
+            return res.status(401).json({error: "Invalid credentials"});
+        }
+
+        const accessToken = generateAccessToken({userName: existingUser.userName, role: existingUser.role});
+        const refreshToken = generateRefreshToken({userName: existingUser.userName, role: existingUser.role});
+        
+        return res.status(200).json({
+            "accessToken": accessToken, "refreshToken": refreshToken
+        });
+
+    } else {
+        return res.status(401).json({error: "Invalid method"});
+    }
+}
---
+++ pages/api/users/refresh.js
@@ -0,0 +1,8 @@
+import { prisma } from "@/prisma/client";
+import { verifyRefreshToken, generateAccessToken} from "@/utils/auth";
+
+export default function handler(req, res) {
+    
+
+
+}
