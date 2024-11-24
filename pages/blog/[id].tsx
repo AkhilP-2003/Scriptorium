3908aced613cb1d25db0d7a1725b657ade81d8b5
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2024-11-24 03:03:50 -0500

    changes to blog stuff:

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import {useRouter} from "next/router"
 
 
-export default function BlogId() {
+export default function CurrentBlogPage() {
+  const router = useRouter();
+  const {id} = router.query; // this is the current blog we are on.
 
   return (
-    <>
-    
-    </>
+
+    <div>this is blog {id}
+    </div>
+
   )
 } 
